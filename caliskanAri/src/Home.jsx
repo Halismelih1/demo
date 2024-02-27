bc4cc93a8c3fd0d701c2cfc8dc08e77da65a290f
Halismelih1 <hhlsabkk@icloud.com>
2024-02-27 21:50:06 +0300

    .

--- caliskanAri/src/Home.jsx
+++ caliskanAri/src/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
       </div>
       <hr />
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4 items-center  '>
         {mockData.map((item) => (
           <div key={item.id}>
             <div>
